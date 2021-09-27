f12a556ecd7245608776be488e79ea3011ae185b
D:\심미영_웹개발_210520 <mythe1004@naver.com>
2021-09-27 20:26:14 +0900

    21-09-27 marondalgram 환경설정 test, DB/JSP 연동

---
+++ src/main/java/com/marondalgram/test/TestController.java
@@ -0,0 +1,37 @@
+package com.marondalgram.test;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.marondalgram.test.bo.TestBO;
+
+@Controller
+public class TestController {
+	// bo 연결
+	@Autowired
+	private TestBO testBO;
+	
+	@RequestMapping("/test1")
+	@ResponseBody
+	public String test1() {
+		return "hello world!!!";
+	}
+	
+	// DB 연동 테스트
+	@RequestMapping("/test2")
+	@ResponseBody
+	public List<Map<String, Object>> test2(){
+		return testBO.getUserList();
+	}
+	
+	// jsp 연동 테스트
+	@RequestMapping("/test3")
+	public String test3() {
+		return "test/test";
+	}
+}
